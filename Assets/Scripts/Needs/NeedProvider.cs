e81d7c50404efb480a710715b785e41023836c32
Jeroen <jeroen@vandegeest.eu>
2024-08-17 17:29:20 +0200

    Fix falling off

--- Assets/Scripts/Needs/NeedProvider.cs
+++ Assets/Scripts/Needs/NeedProvider.cs
@@ -8,18 +8,43 @@ public class NeedProvider : MonoBehaviour
     [SerializeField] private NeedPair[] _needs;
     [SerializeField] private NeedPair[] _provides;
 
+    private int[] _needValues;
+    private int[] _providesValues;
+
     private void OnEnable()
     {
         NeedManager needManager = NeedManager.instance;
 
-        foreach (NeedPair pair in _needs) {
+        _needValues = new int[_needs.Length];
+        _providesValues = new int[_provides.Length];
+
+        for (int i = 0; i < _needs.Length; i++) {
+            NeedPair pair = _needs[i];
             int amount = pair.value.Random();
+            _needValues[i] = amount;
             needManager.ConsumeNeed(pair.need, amount);
         }
 
-        foreach (NeedPair pair in _provides) {
+        for (int i = 0; i < _provides.Length; i++) {
+            NeedPair pair = _provides[i];
             int amount = pair.value.Random();
+            _providesValues[i] = amount;
             needManager.AddNeed(pair.need, amount);
+        }
+    }
+
+    private void OnDisable()
+    {
+        NeedManager needManager = NeedManager.instance;
+
+        for (int i = 0; i < _needs.Length; i++) {
+            NeedPair pair = _needs[i];
+            needManager.ConsumeNeed(pair.need, -_needValues[i]);
+        }
+
+        for (int i = 0; i < _provides.Length; i++) {
+            NeedPair pair = _provides[i];
+            needManager.AddNeed(pair.need, -_providesValues[i]);
         }
     }
 }
